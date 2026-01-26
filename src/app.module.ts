0392280837815b26137dbf1f2024242f41af870c
Sergio Barreras González <sergiobg.isc@gmail.com>
2026-01-25 19:01:56 -0600

    feat(products): add products module with controller and service

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,12 +1,13 @@
 import { Module } from '@nestjs/common';
 import { AuthModule } from './auth/auth.module';
+import { BranchesModule } from './branches/branches.module';
 import { CommonModule } from './common/common.module';
 import { CustomJwtModule } from './custom-jwt/custom-jwt.module';
 import { CustomPassportModule } from './custom-passport/custom-passport.module';
 import { CustomThrottlerModule } from './custom-throttler/custom-throttler.module';
 import { DatabaseModule } from './database/database.module';
+import { ProductsModule } from './products/products.module';
 import { UsersModule } from './users/users.module';
-import { BranchesModule } from './branches/branches.module';
 
 @Module({
   imports: [
@@ -18,6 +19,7 @@ import { BranchesModule } from './branches/branches.module';
     DatabaseModule,
     UsersModule,
     BranchesModule,
+    ProductsModule,
   ],
   controllers: [],
   providers: [],
