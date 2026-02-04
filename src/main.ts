2e0cade7f68acdca5b69a577e7ee20d12d59fcbc
Sergio Barreras González <sergiobg.isc@gmail.com>
2026-02-04 10:49:40 -0600

    feat(swagger): integrate Swagger for API documentation and add Bearer authentication

--- src/main.ts
+++ src/main.ts
@@ -1,6 +1,7 @@
 import { Logger, ValidationPipe } from '@nestjs/common';
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
 
 async function bootstrap() {
   const logger = new Logger('MainLogger');
@@ -16,6 +17,26 @@ async function bootstrap() {
       transform: true,
     }),
   );
+
+  const config = new DocumentBuilder()
+    .setTitle('Magnolias API')
+    .setDescription('API documentation for the Magnolias application')
+    .setVersion('1.0')
+    .addTag('magnolias')
+    .addBearerAuth(
+      {
+        type: 'http',
+        scheme: 'bearer',
+        bearerFormat: 'JWT',
+        name: 'Authorization',
+        in: 'header',
+      },
+      'access-token',
+    )
+    .build();
+
+  const document = SwaggerModule.createDocument(app, config);
+  SwaggerModule.setup('docs', app, document);
 
   const PORT = process.env.PORT ?? 3000;
 
