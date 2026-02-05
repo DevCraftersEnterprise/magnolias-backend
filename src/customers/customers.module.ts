7f63f199bb4afe875cb3f19954f4e336ddd57d00
Sergio Barreras González <sergiobg.isc@gmail.com>
2026-02-05 11:59:27 -0600

    feat(customers): implement customers controller and module with CRUD operations and filters

--- src/customers/customers.module.ts
+++ src/customers/customers.module.ts
@@ -1,9 +1,19 @@
 import { Module } from '@nestjs/common';
-import { CustomersService } from './customers.service';
+import { TypeOrmModule } from '@nestjs/typeorm';
+import { CommonModule } from '../common/common.module';
+import { CustomJwtModule } from '../custom-jwt/custom-jwt.module';
 import { CustomersController } from './customers.controller';
+import { CustomersService } from './customers.service';
+import { Customer } from './entities/customer.entity';
 
 @Module({
   controllers: [CustomersController],
   providers: [CustomersService],
+  imports: [
+    TypeOrmModule.forFeature([Customer]),
+    CommonModule,
+    CustomJwtModule,
+  ],
+  exports: [TypeOrmModule, CustomersService],
 })
 export class CustomersModule {}
