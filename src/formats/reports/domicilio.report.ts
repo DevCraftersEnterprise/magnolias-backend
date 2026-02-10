24cd27b1ff4faee4975f8c72bd2d451191a98161
Sergio Barreras González <sergiobg.isc@gmail.com>
2026-02-09 18:32:06 -0600

    feat(report): add detailed order description table to domicilio report

--- src/formats/reports/domicilio.report.ts
+++ src/formats/reports/domicilio.report.ts
@@ -226,6 +226,143 @@ export const getDomicilioReport = (): TDocumentDefinitions => {
               },
               { text: '', fontSize: 10, bold: false },
             ],
+            [
+              {
+                text: 'NOTA',
+                fontSize: 10,
+                bold: true,
+                fillColor: '#CFCFCF',
+                border: [true, true, true, false],
+              },
+              {
+                text: '',
+                fontSize: 10,
+                bold: false,
+                colSpan: 3,
+                border: [true, true, true, false],
+              },
+              {},
+              {},
+            ],
+          ],
+        },
+      },
+      // Título descripción del pedido
+      {
+        table: {
+          widths: '*',
+          body: [
+            [
+              {
+                text: 'DESCRIPCIÓN DEL PEDIDO',
+                fontSize: 9,
+                bold: true,
+                fillColor: '#A1A1A1',
+                alignment: 'center',
+                border: [true, true, true, false],
+              },
+            ],
+          ],
+        },
+        margin: [0, 0, 0, 0],
+      },
+      // Tabla de descripción del pedido
+      {
+        table: {
+          widths: ['20%', '30%', '20%', '30%'],
+          body: [
+            [
+              {
+                text: 'TAMAÑO',
+                fontSize: 11,
+                bold: true,
+                fillColor: '#CFCFCF',
+              },
+              { text: '', fontSize: 10, bold: false },
+              {
+                text: 'CUBIERTA SABOR',
+                fontSize: 11,
+                bold: true,
+                fillColor: '#CFCFCF',
+              },
+              { text: '', fontSize: 10, bold: false },
+            ],
+            [
+              {
+                text: 'SABOR DE PAN',
+                fontSize: 11,
+                bold: true,
+                fillColor: '#CFCFCF',
+              },
+              { text: '', fontSize: 10, bold: false },
+              {
+                text: 'TIPO DE CUBIERTA',
+                fontSize: 11,
+                bold: true,
+                fillColor: '#CFCFCF',
+              },
+              { text: '', fontSize: 10, bold: false },
+            ],
+            [
+              {
+                text: 'RELLENO',
+                fontSize: 11,
+                bold: true,
+                fillColor: '#CFCFCF',
+              },
+              { text: '', fontSize: 10, bold: false },
+              {
+                text: '"ESCRITO"',
+                fontSize: 11,
+                bold: true,
+                fillColor: '#CFCFCF',
+              },
+              { text: '', fontSize: 10, bold: false },
+            ],
+            [
+              {
+                text: 'ESTILO Y COLOR DEL ESCRITO',
+                fontSize: 11,
+                bold: true,
+                fillColor: '#CFCFCF',
+              },
+              { text: '', fontSize: 10, bold: false },
+              {
+                text: 'UBICACIÓN DEL ESCRITO',
+                fontSize: 11,
+                bold: true,
+                fillColor: '#CFCFCF',
+              },
+              { text: '', fontSize: 10, bold: false },
+            ],
+            [
+              {
+                text: 'ESTILO Y COLOR DE POMPEADO',
+                fontSize: 11,
+                bold: true,
+                fillColor: '#CFCFCF',
+                border: [true, true, true, false],
+              },
+              {
+                text: '',
+                fontSize: 10,
+                bold: false,
+                border: [true, true, true, false],
+              },
+              {
+                text: 'POSICIÓN POMPEADO',
+                fontSize: 11,
+                bold: true,
+                fillColor: '#CFCFCF',
+                border: [true, true, true, false],
+              },
+              {
+                text: '',
+                fontSize: 10,
+                bold: false,
+                border: [true, true, true, false],
+              },
+            ],
           ],
         },
       },
