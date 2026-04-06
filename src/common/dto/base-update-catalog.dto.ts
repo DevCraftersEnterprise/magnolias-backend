a097b1fec95ecb4380f4f47c7f5649dcf1d2bf32
Sergio Barreras González <sergiobg.isc@gmail.com>
2026-04-06 15:39:21 -0600

    refactor: replace individual DTOs and entities with base catalog classes for consistency

--- src/common/dto/base-update-catalog.dto.ts
+++ src/common/dto/base-update-catalog.dto.ts
@@ -1,4 +1,15 @@
 import { PartialType } from "@nestjs/mapped-types";
 import { BaseCreateCatalogDto } from "./base-create-catalog.dto";
+import { ApiProperty } from "@nestjs/swagger";
+import { IsOptional, IsBoolean } from "class-validator";
 
-export class BaseUpdateCatalogDto extends PartialType(BaseCreateCatalogDto) { }
+export class BaseUpdateCatalogDto extends PartialType(BaseCreateCatalogDto) {
+    @ApiProperty({
+        description: 'Indicates if the catalog item is active',
+        example: true,
+        required: false,
+    })
+    @IsOptional()
+    @IsBoolean({ message: 'isActive must be a boolean value' })
+    isActive?: boolean;
+}
