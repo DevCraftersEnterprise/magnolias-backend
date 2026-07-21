22811f54daf8594661de213e274f719473b45279
Sergio Barreras González <sergiobg.isc@gmail.com>
2026-07-21 11:03:22 -0600

    feat: implement flavor use cases with tests and refactor for catalog abstraction

--- src/flavors/usecases/create-flavor.usecase.ts
+++ src/flavors/usecases/create-flavor.usecase.ts
@@ -1,42 +1,18 @@
-import { ConflictException, Injectable, Logger } from '@nestjs/common';
+import { Injectable, Logger } from '@nestjs/common';
 import { InjectRepository } from '@nestjs/typeorm';
 import { Repository } from 'typeorm';
-import { User } from '../../users/entities/user.entity';
-import { CreateFlavorDto } from '../dto/create-flavor.dto';
 import { Flavor } from '../entities/flavor.entity';
+import { BaseCreateCatalogUseCase } from '../../common/usecases/base-create-catalog.usecase';
 
 @Injectable()
-export class CreateFlavorUseCase {
-  private readonly logger = new Logger(CreateFlavorUseCase.name);
+export class CreateFlavorUseCase extends BaseCreateCatalogUseCase<Flavor> {
+  protected readonly logger = new Logger(CreateFlavorUseCase.name);
+  protected readonly entityName = 'Flavor';
 
   constructor(
     @InjectRepository(Flavor)
-    private readonly flavorRepository: Repository<Flavor>,
-  ) {}
-
-  async execute(createFlavorDto: CreateFlavorDto, user: User): Promise<Flavor> {
-    const { name } = createFlavorDto;
-
-    const duplicatedFlavor = await this.flavorRepository.findOne({
-      where: { name: name.toUpperCase() },
-    });
-
-    if (duplicatedFlavor) {
-      this.logger.log(`Duplicated flavor name: ${name}`);
-      throw new ConflictException(`Flavor with name ${name} already exists`);
-    }
-
-    const flavor = this.flavorRepository.create({
-      ...createFlavorDto,
-      name: name.toUpperCase(),
-      createdBy: user,
-      updatedBy: user,
-    });
-
-    const savedFlavor = await this.flavorRepository.save(flavor);
-
-    this.logger.log(`Flavor created with ID: ${savedFlavor.id}`);
-
-    return savedFlavor;
+    repository: Repository<Flavor>,
+  ) {
+    super(repository);
   }
 }
--- src/flavors/usecases/find-all-flavors.usecase.ts
+++ src/flavors/usecases/find-all-flavors.usecase.ts
@@ -1,53 +1,19 @@
 import { Injectable, Logger } from '@nestjs/common';
 import { InjectRepository } from '@nestjs/typeorm';
 import { Repository } from 'typeorm';
-import { PaginationDto } from '../../common/dto/pagination.dto';
-import { PaginationResponse } from '../../common/responses/pagination.response';
+import { BaseFindAllCatalogUseCase } from '../../common/usecases/base-find-all-catalog.usecase';
 import { Flavor } from '../entities/flavor.entity';
 
 @Injectable()
-export class FindAllFlavorsUseCase {
-  private readonly logger = new Logger(FindAllFlavorsUseCase.name);
+export class FindAllFlavorsUseCase extends BaseFindAllCatalogUseCase<Flavor> {
+  protected readonly logger = new Logger(FindAllFlavorsUseCase.name);
+  protected readonly entityName = 'Flavor';
 
   constructor(
     @InjectRepository(Flavor)
-    private readonly flavorRepository: Repository<Flavor>,
-  ) {}
+    repository: Repository<Flavor>,
+  ) {
+    super(repository);
 
-  async execute(
-    paginationDto: PaginationDto,
-  ): Promise<PaginationResponse<Flavor> | Flavor[]> {
-    const { limit, offset } = paginationDto;
-
-    const [flavors, total] = await this.flavorRepository.findAndCount({
-      select: {
-        id: true,
-        name: true,
-        description: true,
-        isActive: true,
-      },
-      take: limit,
-      skip: offset,
-      order: { name: 'ASC' },
-    });
-
-    if (limit !== undefined && offset !== undefined) {
-      this.logger.log(`Found ${flavors.length} flavors with pagination`);
-
-      return {
-        items: flavors,
-        total,
-        pagination: {
-          limit,
-          offset,
-          totalPages: Math.ceil(total / limit),
-          currentPage: Math.floor(offset / limit) + 1,
-        },
-      };
-    }
-
-    this.logger.log(`Found ${flavors.length} flavors without pagination`);
-
-    return flavors;
   }
 }
